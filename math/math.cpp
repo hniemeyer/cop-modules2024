778819a12c58a6c00bad79f354635e60cee19096
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-05-03 07:45:38 +0200

    remove comments

--- math/math.cpp
+++ math/math.cpp
@@ -1,8 +1,6 @@
-// Global module fragment where #includes can happen
 module;
 #include <iostream>
 
-// first thing after the Global module fragment must be a module command
 export module hn.math;
 export import :arithmetic;
 
