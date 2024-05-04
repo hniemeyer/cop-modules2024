547ab8391f30b6e125ff7aaba9dbf6b46822eb8e
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-05-04 13:48:59 +0200

    more tests

--- math_test/math_test.cpp
+++ math_test/math_test.cpp
@@ -4,6 +4,13 @@
 import hn.math;
 
 TEST_CASE("check_add") {
-	CHECK(hn::math::add(5, 6) == 11);
+	CHECK_EQ(hn::math::add(5, 6), 11);
+	CHECK_EQ(hn::math::add(5.1, 6.2), doctest::Approx(11.3));
+}
+
+TEST_CASE("check_subtract")
+{
+	CHECK_EQ(hn::math::subtract(5, 6), -1);
+	CHECK_EQ(hn::math::subtract(5.1, 6.2), doctest::Approx(-1.1));
 }
 
