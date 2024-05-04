db9b89bf3a2d8f2f0d9e601ea5f0990560b7a49c
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-05-04 13:29:06 +0200

    make everything constexpr

--- math/math_arithmetic.cpp
+++ math/math_arithmetic.cpp
@@ -4,22 +4,22 @@ export module hn.math:arithmetic;
 
 namespace hn::math
 {
-	export int add(const int a, const int b)
+	export constexpr int add(const int a, const int b)
 	{
 		return a + b;
 	}
 
-	export int subtract(const int a, const int b)
+	export constexpr int subtract(const int a, const int b)
 	{
 		return a - b;
 	}
 
-	export int multiply(const int a, const int b)
+	export constexpr int multiply(const int a, const int b)
 	{
 		return a * b;
 	}
 
-	export int divide(const int a, const int b)
+	export constexpr int divide(const int a, const int b)
 	{
 		return a / b;
 	}
