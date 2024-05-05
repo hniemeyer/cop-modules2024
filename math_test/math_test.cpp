b0513605a45f21cd7be3f912e0428addbb4a85b9
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-05-05 21:55:40 +0200

    tests

--- math_test/math_test.cpp
+++ math_test/math_test.cpp
@@ -10,3 +10,10 @@ TEST_CASE("check_average")
 	std::vector<double> w = { 1.1, 2.2, 3.3, 4.4, 5.5 };
 	CHECK_EQ(hn::math::average(w), doctest::Approx(3.3));
 }
+
+TEST_CASE("check_distance")
+{
+	hn::math::Point p1 = { 1., 2. };
+	hn::math::Point p2 = { 1.1, 2.2 };
+	CHECK_EQ(hn::math::distance(p1, p2), doctest::Approx(0.223607));
+}
