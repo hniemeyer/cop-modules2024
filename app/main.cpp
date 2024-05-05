f0131ddb54128fe353fe7445c454cd095e131f78
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2024-05-05 21:56:55 +0200

    constexpr 2

--- app/main.cpp
+++ app/main.cpp
@@ -6,8 +6,8 @@ int main()
 {
 	const std::vector a = { 1., 2., 3., 4., 5. };
 	hn::math::print(hn::math::average(a));
-	const hn::math::Point p1 = { 1., 2. };
-	const hn::math::Point p2 = { 1.1, 2.2 };
+	constexpr hn::math::Point p1 = { 1., 2. };
+	constexpr hn::math::Point p2 = { 1.1, 2.2 };
 	hn::math::print(hn::math::distance(p1, p2));
 	return 0;
 }
