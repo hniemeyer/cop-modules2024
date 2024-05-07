da4fa769a232cf03b8525059db70a10504f1d474
Hendrik Niemeyer <hniemeyer@rosen-group.com>
2024-05-07 07:42:39 +0200

    delete implementation file

--- math/math.cpp
+++ math/math.cpp
@@ -5,3 +5,18 @@ export import :statistics;
 export import :print;
 export import :concepts;
 export import :geometry;
+
+namespace hn::math
+{
+	export Point make_point(const double x, const double y);
+}
+
+module :private;
+
+namespace hn::math
+{
+	Point make_point(const double x, const double y)
+	{
+		return Point{ x, y + 6.0 };
+	}
+}
